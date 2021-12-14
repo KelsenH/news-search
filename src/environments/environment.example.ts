54eabdc5b594a0226ffb8fd2ca30fd95bd779244
Kelsen Hazelwood <kelsenhazelwood@gmail.com>
2021-12-14 10:38:45 -0500

    adding env to gitignore

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   newsApiUrl: 'https://newsapi.org/v2/everything',
-  apiKey: '0f637bb9006940e8b5a625a7f64afcfe'
+  apiKey: 'Get this from News Api'
 };
